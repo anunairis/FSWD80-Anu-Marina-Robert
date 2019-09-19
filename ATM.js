13b8976a88e1f978600092d6f5bcacc3a537bf0e
Robert Flunger <robert.flunger@gmx.at>
2019-09-19 15:06:28 +0200

    finished ATM exercise

--- ATM.js
+++ ATM.js
@@ -1,15 +1,61 @@
 
-var X = 5000;
+var initialX = prompt("Type a number");
 
-var A = 10;
+var A = 1;
 var B = 20;
 var C = 20;
 var D = 30;
 var sum = A*100 + B*50 + C*20 + D*10;
 
-document.write(sum)
 console.log(sum)
 
-if (sum < X) {
-	document.write("Sorry, the amount you inquired is not available")
+if (sum < initialX) {
+	document.write("Sorry, the amount you inquired is not available right now.")
 }
+
+if (A > 0) {
+	var a = Math.floor(initialX/100);
+		if (A-a < 0) {
+				a = A;
+			} 			
+}
+
+X = initialX - a*100
+
+if (B > 0) {
+	var b = Math.floor(X/50);
+		if (B-b < 0) {
+				b = B;
+			} 			
+}
+
+X = X - b*50
+
+if (C > 0) {
+	var c = Math.floor(X/20);
+		if (C-c < 0) {
+				c = C;
+			} 			
+}
+
+X = X - c*20
+
+if (D > 0) {
+	var d = Math.floor(X/10);
+		if (D-d < 0) {
+				d = D;
+			} 			
+}
+
+finalX = X - d*10
+
+if (finalX == 0) {
+	document.write(initialX + "€ as: " + a + "x100€| " + b +" x50€| " + c +" x20€| " + d +" x10€" )
+} 
+else 
+{
+	document.write("We're sorry, the amount you inquired is not available right now.")
+}
+
+
+
--- Challenge_Robert.js
+++ Challenge_Robert.js
@@ -1,17 +1,13 @@
+
 var viennalove =[["Vienna", " ", " ", " ", "<br>"],
 				[" ", "is", " ", " ", "<br>"],
 				[" ", " ", "a", " ", "<br>"],
 				[" ", " ", " ", "nice", "<br>"],
 				[" ", " ", " ", " ", "city"]];
 
-
-
 document.write(viennalove[0][0] + " " + viennalove[1][1] + " " + viennalove[2][2]+" "+viennalove[3][3] +" "+viennalove[4][4] + "<br>")
 
-
-
-
 var sentence = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$\"properties $flow$down;$actions$flow$up\"."
-var rep = sentence.replace(/[^a-zA-Z 0-9 , ' " . ; ]+/g, ' ')// replace(/\$/g," ")
+var rep = sentence.replace(/[^a-zA-Z 0-9 , ' " . ; ]+/g, ' ')// replace(/\$/g," ") /$
 var test2 = sentence.split("$").join(" ");
 document.write(rep)
